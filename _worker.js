b8d458f21a29650b7e11f9ceae66fa7ba776b4ef
KurosakiToya <125438694+kurosakikazuto@users.noreply.github.com>
2024-10-05 08:32:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'e7966f2d-7045-442a-a0da-c4ea91b628bd';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
